a492585c5f965910709f7227ad29d4932b645162
saba-karbakhsh <143356265+saba-karbakhsh@users.noreply.github.com>
2025-03-16 06:31:33 -0700

    Update server.js

--- server.js
+++ server.js
@@ -16,13 +16,20 @@ con.connect(function (err) {
 con.query("CREATE DATABASE IF NOT EXISTS DB", function (err, result) {
     if (err) throw err;
     console.log("Database created");
-    con.query("USE DB", function (err, result) {
+});
+con.query("USE DB", function (err, result) {
         con.query("CREATE TABLE IF NOT EXISTS Users (userID INT AUTO_INCREMENT PRIMARY KEY,email VARCHAR(100) NOT NULL UNIQUE,password VARCHAR(255) NOT NULL,role VARCHAR(100) DEFAULT 'user',apiCounter INT DEFAULT 20)ENGINE=InnoDB", function (err, result) {
             if (err) throw err;
             console.log("Table created");
         });
     });
-});
+let userEmails = [];
+    con.query("SELECT email FROM Users", function (err, result) {
+        if (err) throw err;
+        for (let i = 0; i < result.length; i++) {
+            userEmails.push(result[i].email);
+        }
+    }); 
 http.createServer(function (req, res) {
 
     let q = url.parse(req.url, true);
@@ -34,6 +41,10 @@ http.createServer(function (req, res) {
         });
         req.on('end', () => {
             let userData = JSON.parse(body);
+            if (userEmails.includes(userData.email)) {
+                  res.end("Email already exists");
+                return;
+            }else{
             let hashedPassword = crypto.createHash('sha256').update(userData.password).digest('hex');
             userData.password = hashedPassword;
             let sql = "INSERT INTO Users (email, password) VALUES ?";
@@ -42,6 +53,7 @@ http.createServer(function (req, res) {
                 if (err) throw err;
                 res.end("User inserted");
             });
+            }
         });
     } else if (req.method === "POST" && q.pathname === "/login") {
         res.writeHead(200, { 'Content-Type': 'text/plain', 'Access-Control-Allow-Origin': '*' });
